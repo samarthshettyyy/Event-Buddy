6ba3f7ae531d65ad92d1973bebc51704b4aad1f1
V-A-T-S-A-L <vatsalshah004@gmail.com>
2024-09-28 00:29:18 +0530

    create event

---
+++ backend/db/Task.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    assignee: {
+        type: String,
+        required: true
+    },
+    dueDate: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        default: 'todo'
+    },
+    eventId: {
+        type: String,
+        required: true
+    }
+    // Remove or update the email field if not needed
+});
+
+module.exports = mongoose.model('Task', taskSchema);
