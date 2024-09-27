242a3ffdb310453f5cb03f3e9c205b08c9893e1f
samarthshettyyy <samarthshetty04@gmail.com>
2024-09-27 18:41:22 +0530

    car

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,41 @@
 import './App.css';
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
-import Nav from './components/Nav'
+import Nav from './components/Nav';
 import PrivateComponent from './components/PrivateComponent';
 import Signup from './components/Signup';
 import FlowCarousel from './components/FlowCarousel';
+import CreateEvent from './components/CreateEvent';
+import MyEvents from './components/MyEvents';
 
 import 'flowbite';
 import './index.css';
 
-
-
 function App() {
   return (
     <div className="App">
-      
       <BrowserRouter>
         <Nav />
         <Routes>
-          
           <Route element={<PrivateComponent />}>
-            <Route path='/' element={<h1>HomePage After Login</h1>}/>
-            <Route path='/create-event' element={<CreateEvent />}/>
-            <Route path='/my-events/:id' element={<MyEvents />}/>
+            {/* HomePage After Login with FlowCarousel */}
+            <Route
+              path="/"
+              element={
+                <div>
+                  <h1>HomePage After Login</h1>
+                  <div className="carousel-container absolute right-0 top-1/2 transform -translate-y-1/2 h-1/2 w-1/2 overflow-hidden"> {/* Limit the carousel height */}
+                    <FlowCarousel />
+                  </div>
+                </div>
+              }
+            />
+            <Route path="/create-event" element={<CreateEvent />} />
+            <Route path="/my-events/:id" element={<MyEvents />} />
           </Route>
-          <Route path='/signup' element={<Signup />}/>
-          <Route path='/login' element={<Login />}/>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
-        <FlowCarousel/>
       </BrowserRouter>
     </div>
   );
