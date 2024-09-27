73c806656528876c463d883fc1f920f645df8f2d
samarthshettyyy <samarthshetty04@gmail.com>
2024-09-27 17:38:58 +0530

    home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,22 @@
 import './App.css';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import Login from './components/Login';
-import Nav from './components/Nav';
+import Nav from './components/Nav'
 import PrivateComponent from './components/PrivateComponent';
 import Signup from './components/Signup';
+
+import 'flowbite';
+import './index.css';
+
 
 function App() {
   return (
     <div className="App">
+      
       <BrowserRouter>
         <Nav />
         <Routes>
+          
           <Route element={<PrivateComponent />}>
             <Route path='/' element={<h1>HomePage After Login</h1>}/>
           </Route>
